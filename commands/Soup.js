75753a93b42561944b64d075f5d083e60cac68e1
Ian <i.seto00@gmail.com>
2020-10-01 16:28:45 -0400

    Added command

---
+++ commands/Soup.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    name: 'soup',
+    description: 'im a soup',
+    guildOnly: true,
+
+    execute(message) {
+
+        message.channel.send('https://www.youtube.com/watch?v=vjV8DjQktLU');
+
+    },
+
+};
