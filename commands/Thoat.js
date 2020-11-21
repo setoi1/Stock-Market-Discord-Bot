61d493423f70a34ad5e5c4af2e0ebac8fc5a1580
Ian <i.seto00@gmail.com>
2020-11-21 17:09:36 -0500

    Added Thoat

---
+++ commands/Thoat.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    name: 'dude',
+    description: 'league',
+    guildOnly: true,
+
+    execute(message) {
+
+        message.channel.send('<@485645992193163273> pemis');
+
+    },
+
+};
