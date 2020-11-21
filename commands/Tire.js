5d0bf5016d7291d9d8aa3cfcad636dea2c681c68
Ian <i.seto00@gmail.com>
2020-11-20 22:11:30 -0500

    Added Tire

---
+++ commands/Tire.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    name: 'tire',
+    description: 'im a tire',
+    guildOnly: true,
+
+    execute(message) {
+
+        message.channel.send('https://www.youtube.com/watch?v=c1sAnP0PFC0');
+
+    },
+
+};
