3714653f7eda65cba13232f4601dd23d3a52edc6
Ian <i.seto00@gmail.com>
2021-01-31 23:54:59 -0500

    Updated Weather.js

--- commands/Weather.js
+++ commands/Weather.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = {
 
@@ -11,17 +12,14 @@ module.exports = {
     guildOnly: true,
 
     async execute(message, args) {
-
+        const weatherAPIKey = config.WEATHERAPIKEY;
         const city = args[0];
         const state = args[1];
         const countryCode = args[2];
 
-        const weatherAPIKey = config.WEATHERAPIKEY;
-
         if (args[0] === undefined || args[1] === undefined) return message.reply('Usage: -weather <location> <state initials> <country code>');
 
         try {
-
             const { weather, main, name } = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${countryCode}&appid=${weatherAPIKey}`).then(response => response.json());
 
             const condition = weather[0].main;
@@ -29,31 +27,29 @@ module.exports = {
             const humidity = main.humidity;
             const convertedTempF = Math.round(((mainTemp - 273.15) * 1.8) + 32);
             const convertedTempC = Math.round(mainTemp - 273.15);
-
             const toUpperState = state.toUpperCase();
 
             const embed = new Discord.MessageEmbed()
-            .setColor(0x00AE86)
-            .setAuthor('Jason', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-            .setTitle('Location')
-            .setDescription(`${name}, ${toUpperState}`)
-            .addFields({ name: 'Fahrenheit', value: `${convertedTempF}°` })
-            .addFields({ name: 'Celsius', value: `${convertedTempC}°` })
-            .addFields({ name: 'Condition', value: `${condition}` })
-            .addFields({ name: 'Humidity', value: `${humidity}%` });
+                .setColor(0x00AE86)
+                .setAuthor('Jason', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
+                .setTitle('Current Weather')
+                .setDescription(`${name}, ${toUpperState}`)
+                .addFields(
+                    { name: 'Fahrenheit', value: `${convertedTempF}°`, inline: true },
+                    { name: 'Celsius', value: `${convertedTempC}°`, inline: true },
+                )
+                .addFields(
+                    { name: 'Condition', value: `${condition}` },
+                    { name: 'Humidity', value: `${humidity}%`, inline: true },
+                )
+                .setFooter('Hunter is gay');
 
             message.channel.send(embed);
-
         }
 
         catch (error) {
-
             console.log(error);
-
             message.reply('Location does not exist');
-
         }
-
     },
-
 };
