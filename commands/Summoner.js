ed7c92767cb920275446ed92c4421926e5b0507d
Ian <i.seto00@gmail.com>
2021-02-02 00:15:44 -0500

    Updated README.md, Summoner.js, Weather.js

--- commands/Summoner.js
+++ commands/Summoner.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const axios = require('axios');
 const fetch = require('node-fetch');
 const { RIOTAPIKEY } = require('../config.json');
 
@@ -13,207 +14,106 @@ module.exports = {
     async execute(message, args) {
 
         const region = args[0];
-        const inName = args[1];
+        const inputName = args[1];
+
+        var summoner = {};
+        var league = [];
+        var match = {};
 
         if (args[0] === undefined || args[1] === undefined) return message.reply('Usage: -summoner <region> <summoner name>');
 
-        try {
+            // Encrypted Info Fetch
+            if (region === 'NA') {
 
-            // NA
-            if (region === 'NA' || region === 'na') {
-                const { accountId, name, id, summonerLevel } = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inName}?api_key=${RIOTAPIKEY}`).then(response => response.json());
-                console.log('First fetch completed');
-
-                const NAResponse = await fetch(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${RIOTAPIKEY}`).then(response => response.json());
-                console.log('Second fetch completed');
-
-                console.log(NAResponse);
-
-                if (NAResponse === undefined || NAResponse.length == 0) {
-                    const embed = new Discord.MessageEmbed()
-                    .setColor(0x00AE86)
-                    .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                    .setTitle('Player Profile')
-                    .addFields({ name: 'Summoner Name', value: `${name}` })
-                    .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                    .addFields({ name: 'Ranked Solo / Duo', value: 'Unranked' });
-
-                    message.channel.send(embed);
+                // Summoner Fetch
+                try {
+                    const summonerResponse = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inputName}?api_key=${RIOTAPIKEY}`);
+                    summoner = summonerResponse.data;
+                }
+                catch (error) {
+                    console.log(error);
                 }
 
-                else {
-                    const queueType = NAResponse[0].queueType;
-                    const rank = NAResponse[0].rank;
-                    const tier = NAResponse[0].tier;
-                    const leaguePoints = NAResponse[0].leaguePoints;
-                    const wins = NAResponse[0].wins;
-                    const losses = NAResponse[0].losses;
+                // League Fetch
+                try {
+                    const leagueResponse = await axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.id}?api_key=${RIOTAPIKEY}`);
+                    league = leagueResponse.data;
+                }
+                catch (error) {
+                    console.log(error);
+                }
 
-                    let winRatio = 100 * (wins / (wins + losses));
-                    winRatio = winRatio.toFixed(0);
+                // Match Fetch
+                try {
+                    const matchResponse = await axios.get(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summoner.id}?api_key=${RIOTAPIKEY}`);
+                    match = matchResponse.data;
+                }
+                catch (error) {
+                    console.log(error);
+                }
 
-                    console.log(NAResponse);
+                const { accountId, name, profileIconId, summonerLevel  } = summoner;
+                const { mapId, gameMode, gameType } = match;
+                let mapType;
 
-                    if (queueType === 'RANKED_SOLO_5x5') {
-                        const embed = new Discord.MessageEmbed()
+                // Current Map
+                if (mapId === 11) {
+                    mapType = "Summoner's Rift";
+                }
+                else if (mapId === 12) {
+                    mapType = 'Howling Abyss';
+                }
+                else if (!match) {
+                    mapType = 'User is not in a match.';
+                }
+
+                var embed;
+
+                switch (league.length) {
+                    case 0:
+                        embed = new Discord.MessageEmbed()
                         .setColor(0x00AE86)
                         .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
                         .setTitle('Player Profile')
+                        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.2.1/img/profileicon/${profileIconId}.png`)
                         .addFields({ name: 'Summoner Name', value: `${name}` })
-                        .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                        .addFields({ name: 'Ranked Solo / Duo', value: `${tier} ${rank} ${leaguePoints} LP` })
-                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` });
+                        .addFields(
+                            { name: 'Summoner Level', value: `${summoner.summonerLevel}`, inline: true },
+                            { name: 'Current Match', value: `${mapType}`, inline: true },
+                        )
+                        .addFields({ name: 'Rank', value: 'Unranked' })
+                        .setFooter(`https://na.op.gg/summoner/userName=${name}`);
 
                         message.channel.send(embed);
-                    }
+                        break;
+                    case 1:
+                        let { tier, rank, leaguePoints, wins, losses } = league[0];
 
-                    else if (queueType === 'RANKED_FLEX_SR') {
-                        const embed = new Discord.MessageEmbed()
+                        let winRatio = 100 * (wins / (wins + losses));
+                        winRatio = winRatio.toFixed(0);
+
+                        let rankString = (league[0].queueType === 'RANKED_SOLO_5x5') ? 'Ranked Solo / Duo' : 'Ranked Flex 5v5';
+
+                        embed = new Discord.MessageEmbed()
                         .setColor(0x00AE86)
                         .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
                         .setTitle('Player Profile')
+                        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.2.1/img/profileicon/${profileIconId}.png`)
                         .addFields({ name: 'Summoner Name', value: `${name}` })
-                        .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                        .addFields({ name: 'Ranked Flex 5V5', value: `${tier} ${rank} ${leaguePoints} LP` })
-                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` });
+                        .addFields(
+                            { name: 'Summoner Level', value: `${summonerLevel}`, inline: true },
+                            { name: 'Current Match', value: `${mapType}`, inline: true },
+                        )
+                        .addFields({ name: `${rankString}`, value: `${tier} ${rank} ${leaguePoints} LP`, inline: true })
+                        .addFields({ name: `${rankString}`, value: `${tier} ${rank} ${leaguePoints} LP`, inline: true })
+                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` })
+                        .setFooter(`https://na.op.gg/summoner/userName=${name}`);
 
                         message.channel.send(embed);
-                    }
+                        break;
+                    case 2:
+                        
                 }
-            }
-
-            // EUW
-            else if (region === 'EUW' || region === 'euw') {
-                const { accountId, name, id, summonerLevel } = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inName}?api_key=${RIOTAPIKEY}`).then(response => response.json());
-                console.log('First fetch completed');
-
-                const EUResponse = await fetch(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${RIOTAPIKEY}`).then(response => response.json());
-                console.log('Second fetch completed');
-
-                console.log(EUResponse);
-
-                if (EUResponse === undefined || EUResponse.length == 0) {
-                    const embed = new Discord.MessageEmbed()
-                    .setColor(0x00AE86)
-                    .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                    .setTitle('Player Profile')
-                    .addFields({ name: 'Summoner Name', value: `${name}` })
-                    .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                    .addFields({ name: 'Ranked Solo / Duo', value: 'Unranked' });
-
-                    message.channel.send(embed);
-                }
-
-                else {
-                    const queueType = EUResponse[0].queueType;
-                    const rank = EUResponse[0].rank;
-                    const tier = EUResponse[0].tier;
-                    const leaguePoints = EUResponse[0].leaguePoints;
-                    const wins = EUResponse[0].wins;
-                    const losses = EUResponse[0].losses;
-
-                    let winRatio = 100 * (wins / (wins + losses));
-                    winRatio = winRatio.toFixed(0);
-
-                    console.log(EUResponse);
-
-                    if (queueType === 'RANKED_SOLO_5x5') {
-                        const embed = new Discord.MessageEmbed()
-                        .setColor(0x00AE86)
-                        .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                        .setTitle('Player Profile')
-                        .addFields({ name: 'Summoner Name', value: `${name}` })
-                        .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                        .addFields({ name: 'Ranked Solo / Duo', value: `${tier} ${rank} ${leaguePoints} LP` })
-                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` });
-
-                        message.channel.send(embed);
-                    }
-
-                    else if (queueType === 'RANKED_FLEX_SR') {
-                        const embed = new Discord.MessageEmbed()
-                        .setColor(0x00AE86)
-                        .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                        .setTitle('Player Profile')
-                        .addFields({ name: 'Summoner Name', value: `${name}` })
-                        .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                        .addFields({ name: 'Ranked Flex 5V5', value: `${tier} ${rank} ${leaguePoints} LP` })
-                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` });
-
-                        message.channel.send(embed);
-                    }
-                }
-            }
-
-            // KR
-            else if (region === 'KR' || region === 'kr') {
-                const { accountId, name, id, summonerLevel } = await fetch(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inName}?api_key=${RIOTAPIKEY}`).then(response => response.json());
-                console.log('First fetch completed');
-
-                const KRResponse = await fetch(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${RIOTAPIKEY}`).then(response => response.json());
-                console.log('Second fetch completed');
-
-                console.log(KRResponse);
-
-                if (KRResponse === undefined || KRResponse.length == 0) {
-                    const embed = new Discord.MessageEmbed()
-                    .setColor(0x00AE86)
-                    .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                    .setTitle('Player Profile')
-                    .addFields({ name: 'Summoner Name', value: `${name}` })
-                    .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                    .addFields({ name: 'Ranked Solo / Duo', value: 'Unranked' });
-
-                    message.channel.send(embed);
-                }
-
-                else {
-                    const queueType = KRResponse[0].queueType;
-                    const rank = KRResponse[0].rank;
-                    const tier = KRResponse[0].tier;
-                    const leaguePoints = KRResponse[0].leaguePoints;
-                    const wins = KRResponse[0].wins;
-                    const losses = KRResponse[0].losses;
-
-                    let winRatio = 100 * (wins / (wins + losses));
-                    winRatio = winRatio.toFixed(0);
-
-                    console.log(KRResponse);
-
-                    if (queueType === 'RANKED_SOLO_5x5') {
-                        const embed = new Discord.MessageEmbed()
-                        .setColor(0x00AE86)
-                        .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                        .setTitle('Player Profile')
-                        .addFields({ name: 'Summoner Name', value: `${name}` })
-                        .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                        .addFields({ name: 'Ranked Solo / Duo', value: `${tier} ${rank} ${leaguePoints} LP` })
-                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` });
-
-                        message.channel.send(embed);
-                    }
-
-                    else if (queueType === 'RANKED_FLEX_SR') {
-                        const embed = new Discord.MessageEmbed()
-                        .setColor(0x00AE86)
-                        .setAuthor('Chineser', 'https://i.gyazo.com/thumb/1200/c3f5dbb6c885e84ca376dce711db2c2a-png.jpg')
-                        .setTitle('Player Profile')
-                        .addFields({ name: 'Summoner Name', value: `${name}` })
-                        .addFields({ name: 'Summoner Level', value: `${summonerLevel}` })
-                        .addFields({ name: 'Ranked Flex 5V5', value: `${tier} ${rank} ${leaguePoints} LP` })
-                        .addFields({ name: 'Win / Loss Ratio', value: `${wins}W / ${losses}L | ${winRatio}%` });
-
-                        message.channel.send(embed);
-                    }
-                }
-            }
-        }
-
-        catch (error) {
-            console.error(error);
-            message.reply('Summoner is not ranked or does not exist.');
-        }
-
-    },
-
+            } // end of if
+    } // end of execute
 };
--- commands/Weather.js
+++ commands/Weather.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 const fetch = require('node-fetch');
-const axios = require('axios');
 
 module.exports = {
 
