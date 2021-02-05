10a7697adca0ddb885d64f05fc98e7f725744403
Ian <i.seto00@gmail.com>
2021-02-05 05:06:01 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -132,6 +132,23 @@ client.on('message', async message => {
 
 });
 
+// Knocking
+client.on('message', async message => {
+
+    if (!message.guild) return;
+
+    if (message.content === '-kabuki') {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            connection.play(ytdl('https://www.youtube.com/watch?v=WkwNS1AsbE0', { filter: 'audioonly' }), { volume: 0.20 });
+        }
+        else {
+            message.reply('You need to be in the voice channel for this command to work');
+        }
+    }
+
+});
+
 // Chat logger
 client.on('message', message => {
 
