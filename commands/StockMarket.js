e3b6699387e48d2b78e88dfbc21f78d5dd6144a5
Ian Seto <i.seto00@gmail.com>
2023-10-27 03:23:28 -0400

    added error handlers and code cleanup

--- commands/StockMarket.js
+++ commands/StockMarket.js
@@ -1,26 +1,27 @@
 const Discord = require('discord.js');
 const axios = require('axios');
-const { POLYGONAPIKEY } = require('../config.json');
+const { polygon } = require('../config.json');
 
 module.exports = {
-
     name: 'stock',
     description: 'stock data',
     usage: '<ticker> <year-month-date>',
     args: true,
     guildOnly: true,
-
     async execute(message, args) {
-        if (args[0] === undefined || args[1] === undefined) return message.reply('Usage: \`-stock <ticker> <news or price> (if price) <YYYY-MM-DD>\`');
+        let ticker = args[1];
+        if (args[0] === undefined) return message.reply('Usage: \`!stock <ticker> <news or price> (if price) <YYYY-MM-DD>\`');
 
         let embed = new Discord.MessageEmbed();
 
         try {
-            switch (args[1]) {
+            switch (args[0]) {
+                /*
                 case 'tickers': // List all Polygon Tickers
                     await axios.get(`https://api.polygon.io/v3/reference/tickers?market=stocks&exchange=XNYS&active=true&apiKey=${POLYGONAPIKEY}`)
                         .then(function (response) {
                             const [{ ticker, name, last_updated_utc }] = response;
+                            console.log(response)
 
                             embed = new Discord.MessageEmbed()
                             .setColor(0x00AE86)
@@ -35,22 +36,23 @@ module.exports = {
                         .catch(function (error) {
                             console.error(error);
                         });
+                */
                 case 'news': // Ticker News
-                    await axios.get(`https://api.polygon.io/v1/meta/symbols/${ticker}/news?perpage=5&page=1&apiKey=${POLYGONAPIKEY}`)
+                    if (args[1] === undefined) return message.reply('Usage: \`!stock news <ticker>\`');
+                    await axios.get(`https://api.polygon.io/v2/reference/news?ticker=${ticker}&apiKey=${polygon}`)
                         .then(function (response) {
-                            const [{ title, source, summary }] = response;
-                            const newsURL = tickerNews[0].url;
+                            //console.log(response.data.results)
+                            const { title, author, published_utc, article_url, image_url, description } = response.data.results[0];
 
                             embed = new Discord.MessageEmbed()
                             .setColor(0x00AE86)
-                            .setThumbnail(`${logo}`)
-                            .setTitle(`${ticker}`)
-                            .setDescription(`${name} | ${url}`)
+                            .setThumbnail(`${image_url}`)
+                            .setTitle(`${title}`)
+                            //.setDescription(`${author}`)
                             .addFields(
-                                { name: 'URL', value: `${newsURL}` },
-                                { name: 'Title', value: `${title}` },
-                                { name: 'Summary', value: `${summary}` },
-                                { name: 'Source', value: `${source}`},
+                                { name: 'Author', value: `${author}` },
+                                { name: 'Summary', value: `${description}` },
+                                { name: 'URL', value: `${article_url}` },
                             );
                             message.channel.send(embed);
                         })
@@ -58,15 +60,14 @@ module.exports = {
                             console.error(error);
                         });
                 case 'price': // Daily Open / Close
+                    if (args[2] === undefined) return message.reply('Usage: \`!stock price <ticker> <YYYY-MM-DD>\`');
                     const date = args[2];
-                    await axios.get(`https://api.polygon.io/v1/open-close/${ticker}/${date}?unadjusted=true&apiKey=${POLYGONAPIKEY}`)
+                    await axios.get(`https://api.polygon.io/v1/open-close/${ticker}/${date}?adjusted=true&apiKey=${polygon}`)
                         .then(function (response) {
-                            const { preMarket, open, low, high, close, volume, afterHours } = response;
+                            const { open, high, low, close, volume, afterHours, preMarket } = response.data;
                             embed = new Discord.MessageEmbed()
                             .setColor(0x00AE86)
-                            .setThumbnail(`${logo}`)
                             .setTitle(`${ticker}`)
-                            .setDescription(`${name} | ${url}`)
                             .addFields(
                                 { name: 'Pre-Market', value: `${preMarket}`, inline: true },
                                 { name: 'Low', value: `${low}`, inline: true },
@@ -77,11 +78,6 @@ module.exports = {
                                 { name: 'High', value: `${high}`, inline: true },
                                 { name: 'After-Hours', value: `${afterHours}`, inline: true },
                             )
-                            .addFields(
-                                { name: 'Volume', value: `${volume}`, inline: true },
-                                { name: 'Market Cap', value: `${marketcap}`, inline: true },
-                                { name: 'Sector', value: `${sector}`, inline: true },
-                            );
                             message.channel.send(embed);
                         })
                         .catch(function (error) {
